58c4f0f652e89fb40b57a83afab2b6c0f87742d5
cosmicbagelcode <nicholastom@outlook.in>
2024-04-12 06:46:38 +0530

    patch: store keys in environment variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'e7351d520e2a4255b10f87de43b7dc4a'
-    }
-  });
+  baseURL: import.meta.env.VITE_BASE_URL,
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY,
+  },
+});
